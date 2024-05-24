8465c03f57cfa5c8493afc9c14c772e48c1159bc
Matteo von Haxthausen <matteovhaxt@gmail.com>
2024-05-24 14:34:16 +0200

    feat: data endpoints

---
+++ apps/api/src/routes/data/index.ts
@@ -0,0 +1,25 @@
+import { App } from "$plugins/index";
+import prisma from "@repo/database/prisma";
+
+const route = (app: App) => app
+    .get("/data", async ({ store }) => {
+        const data = await prisma.value.findMany({
+            where: {
+                workspaceId: store.token.workspaceId,
+            }
+        });
+
+        return data;
+    })
+    .get("/data/:id", async ({ params, store }) => {
+        const data = await prisma.value.findMany({
+            where: {
+                workspaceId: store.token.workspaceId,
+                distinctId: params.id,
+            }
+        });
+
+        return data;
+    });
+
+export default route;
--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -2,11 +2,13 @@ import Elysia from "elysia";
 import agent from "./agent";
 import auth from "./middleware/auth";
 import token from "./token";
+import data from "./data";
 
 const api = new Elysia()
     .use(auth)
     .use(agent)
-    .use(token);
+    .use(token)
+    .use(data);
 
 export default api;
 
