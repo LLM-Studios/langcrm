9b612a5fe4743c54012e725017885358f78b434d
Matteo von Haxthausen <matteovhaxt@gmail.com>
2024-05-24 21:25:51 +0200

    feat: data page

---
+++ apps/app/app/api/data/route.ts
@@ -0,0 +1,37 @@
+import { getUserWorkspace } from "@/lib/supabase/utils";
+import prisma from "@repo/database/prisma";
+
+export async function GET() {
+  const { user, workspace } = await getUserWorkspace();
+  const data = await prisma.value
+    .findMany({
+      where: {
+        workspaceId: workspace!.id,
+      },
+      include: {
+        key: true,
+      },
+    })
+    .catch((err) => {
+      return new Response(
+        JSON.stringify({
+          success: false,
+          error: err,
+        }),
+        {
+          status: 200,
+          statusText: "Data not found",
+        },
+      );
+    });
+  return new Response(
+    JSON.stringify({
+      success: true,
+      data,
+    }),
+    {
+      status: 200,
+      statusText: "Data found",
+    },
+  );
+}
---
+++ apps/app/app/data/DataTable.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import {
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell,
+  Table,
+} from "@/components/ui/table";
+import { Loader2 } from "lucide-react";
+import { useEffect, useState } from "react";
+import { Value as ValueType, Key as KeyType } from "@prisma/client";
+
+type DataType = ValueType & {key: KeyType}
+
+export default function DataTable() {
+  const [data, setData] = useState<DataType[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    setIsLoading(true);
+    const response = await fetch("api/data");
+    if (!response.ok) {
+      throw new Error("Error getting data");
+    }
+    const json = await response.json();
+    if (json.success) {
+        const data = json.data as DataType[]
+        setData(data);
+    }
+    setIsLoading(false);
+  }
+
+  return (
+    <>
+      <div className="flex items-center">
+        <h1 className="font-semibold text-lg md:text-2xl">Data</h1>
+      </div>
+      <div className="border shadow-sm rounded-lg">
+          {isLoading ? (
+            <div className="flex justify-center items-center h-full">
+              <Loader2 className="h-4 w-4 animate-spin" />
+            </div>
+          ) : ( <Layout data={data} /> )}
+      </div>
+    </>
+  );
+}
+
+function Row({data}: {data: DataType}) {
+  return (
+    <TableRow>
+      <TableCell>{data.key.id}</TableCell>
+      <TableCell className="font-medium">{data.value}</TableCell>
+      <TableCell>{data.key.description}</TableCell>
+      <TableCell className="hidden md:table-cell">{data.distinctId}</TableCell>
+      <TableCell className="hidden md:table-cell">{data.createdAt.toString()}</TableCell>
+    </TableRow>
+  );
+}
+
+function Layout({data}: {data: DataType[]}) {
+    return (
+        <><Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="max-w-[150px]">key</TableHead>
+            <TableHead className="max-w-[150px]">value</TableHead>
+            <TableHead className="max-w-[150px]">description</TableHead>
+            <TableHead className="hidden md:table-cell">distinctId</TableHead>
+            <TableHead className="hidden md:table-cell">createdAt</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+            {data.map((item) => (
+                <Row key={item.id} data={item} />
+            ))}
+        </TableBody>
+      </Table>
+        </>
+    )
+}
