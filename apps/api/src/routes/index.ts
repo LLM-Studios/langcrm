e38d5031adba14ecb9f963811260fc573ceddfa9
Matteo von Haxthausen <matteovhaxt@gmail.com>
2024-05-28 16:20:04 +0200

    feat: schema endpoints

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -3,7 +3,8 @@ import agent from "./ingest";
 import auth from "./middleware/auth";
 import token from "./token";
 import data from "./data";
+import schema from "./schema";
 
-const api = new Elysia().use(auth).use(agent).use(token).use(data);
+const api = new Elysia().use(auth).use(agent).use(token).use(data).use(schema);
 
 export default api;
---
+++ apps/api/src/routes/schema/index.ts
@@ -0,0 +1,45 @@
+import { App } from "$plugins/index";
+import prisma from "@repo/database/prisma";
+import { t } from "elysia";
+
+const route = (app: App) =>
+  app
+    .get("/schema", async ({ store }) => {
+      const data = await prisma.key.findMany({
+        where: {
+          workspaceId: store.token.workspaceId,
+        },
+      });
+
+      return data;
+    })
+    .post("/schema", async ({ body, store }) => {
+        const { name, description, type, priority, tags } = body;
+        const data = await prisma.key.create({
+            data: {
+                id: name,
+                workspaceId: store.token.workspaceId,
+                description,
+                type,
+                priority,
+                tags,
+            },
+        });
+        return data;
+        }, {
+            body: t.Object({
+                name: t.String(),
+                description: t.String(),
+                type: t.String(),
+                priority: t.Optional( t.Enum({
+                    REQUIRED: "REQUIRED",
+                    HIGH: "HIGH",
+                    MEDIUM: "MEDIUM",
+                    LOW: "LOW",
+                    VERY_LOW: "VERY_LOW",
+                })),
+                tags: t.Optional(t.Array(t.String())),
+            })
+        });
+
+export default route;
