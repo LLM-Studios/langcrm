2df73e3257ae3e7853410b15002500b5ed4c3e89
Matteo von Haxthausen <matteovhaxt@gmail.com>
2024-07-03 11:15:09 +0200

    fix: token and workspaceId

--- apps/app/app/api/data/route.ts
+++ apps/app/app/api/data/route.ts
@@ -1,41 +1,47 @@
-import { getUserWorkspace } from "@/lib/supabase/utils";
+import { getUser } from "@/lib/supabase/utils";
 import { prisma } from "@repo/database/prisma";
+import { NextResponse } from "next/server";
 
 export async function GET() {
-	const workspace = await getUserWorkspace();
-	const data = await prisma.value
-		.findMany({
-			where: {
-				workspaceId: workspace!.id,
-			},
-			orderBy: {
-				createdAt: "desc",
-			},
-			distinct: "keyId",
-			include: {
-				key: true,
-			},
-		})
-		.catch((err: Error) => {
-			return new Response(
-				JSON.stringify({
-					success: false,
-					error: err,
-				}),
-				{
-					status: 200,
-					statusText: "Data not found",
-				}
-			);
-		});
-	return new Response(
-		JSON.stringify({
-			success: true,
-			data,
-		}),
-		{
-			status: 200,
-			statusText: "Data found",
-		}
-	);
+  const user = await getUser();
+  const workspaceId = user?.workspaces[0]?.id;
+  if (!workspaceId) {
+    return NextResponse.json(
+      { error: "No workspaceId provided" },
+      { status: 400 },
+    );
+  }
+  const data = await prisma.value
+    .findMany({
+      where: {
+        workspaceId: workspaceId,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+      distinct: "keyId",
+      include: {
+        key: true,
+      },
+    });
+  if (!data) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: "Data not found",
+      },
+      {
+        status: 404,
+      },
+    );
+  }
+  return NextResponse.json(
+    {
+      success: true,
+      data,
+    },
+    {
+      status: 200,
+    },
+  );
 }
--- apps/app/app/api/token/route.ts
+++ apps/app/app/api/token/route.ts
@@ -1,97 +1,130 @@
-import { getUserWorkspace } from "@/lib/supabase/utils";
-import { Token, prisma } from "@repo/database/prisma";
+import { getUser } from "@/lib/supabase/utils";
+import { prisma } from "@repo/database/prisma";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
-	const { user, workspace } = await getUserWorkspace();
-	const tokens = await prisma.token
-		.findMany({
-			where: {
-				userId: user!.id,
-				workspaceId: workspace!.id,
-			},
-			select: {
-				id: true,
-				value: true,
-				createdAt: true,
-			},
-		})
-		.catch((err: Error) => {
-			throw new Error(
-				JSON.stringify({
-					error: err,
-				})
-			);
-		});
-	return new Response(
-		JSON.stringify({
-			tokens,
-		}),
-		{
-			status: 200,
-			statusText: "Tokens found",
-		}
-	);
+  const user = await getUser();
+  const workspaceId = user?.workspaces[0]?.id;
+  if (!workspaceId) {
+    return NextResponse.json(
+      { error: "No workspaceId provided" },
+      { status: 400 },
+    );
+  }
+  const tokens = await prisma.token
+    .findMany({
+      where: {
+        userId: user!.id,
+        workspaceId,
+      },
+      select: {
+        id: true,
+        value: true,
+        createdAt: true,
+      },
+    });
+  if (!tokens) {
+    return NextResponse.json(
+      { error: "Tokens not found" },
+      { status: 404 },
+    );
+  }
+  return NextResponse.json(
+    {
+      tokens,
+    },
+    {
+      status: 200,
+      statusText: "Tokens found",
+    },
+  );
 }
 
 export async function POST() {
-	const { user, workspace } = await getUserWorkspace();
-	const response = await fetch(
-		`${process.env.API_URL}/token/${process.env.DOPPLER_ENVIRONMENT}`,
-		{
-			headers: {
-				Authorization: `Bearer ${process.env.API_KEY}`,
-			},
-		}
-	).catch((err: Error) => {
-		throw new Error("Error generating token", err);
-	});
-	const data = await response.json();
-	const token = await prisma.token
-		.create({
-			data: {
-				value: data.token,
-				userId: user!.id,
-				workspaceId: workspace!.id,
-			},
-		})
-		.catch((err: Error) => {
-			throw new Error("Error creating token", err);
-		});
-	return new Response(JSON.stringify(token), {
-		status: 200,
-		statusText: "Token created",
-	});
+  const user = await getUser();
+  const workspaceId = user?.workspaces[0]?.id;
+  if (!workspaceId) {
+    return NextResponse.json(
+      { error: "No workspaceId provided" },
+      { status: 400 },
+    );
+  }
+  const response = await fetch(
+    `${process.env.API_URL}/token/${process.env.DOPPLER_ENVIRONMENT}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.API_KEY}`,
+      },
+    },
+  );
+  if (!response.ok) {
+    return NextResponse.json(
+      { error: "Error generating token" },
+      { status: 500 },
+    );
+  }
+  const data = await response.json();
+  const token = await prisma.token
+    .create({
+      data: {
+        value: data.token,
+        userId: user!.id,
+        workspaceId,
+      },
+    });
+  if (!token) {
+    return NextResponse.json(
+      { error: "Token not created" },
+      { status: 500 },
+    );
+  }
+  return NextResponse.json(token, {
+    status: 200,
+  });
 }
 
 export async function DELETE() {
-	const { user, workspace } = await getUserWorkspace();
-	const token = await prisma.token
-		.findFirst({
-			where: {
-				userId: user!.id,
-				workspaceId: workspace!.id,
-			},
-		})
-		.catch((err: Error) => {
-			throw new Error("Error getting token", err);
-		});
-	await prisma.token
-		.delete({
-			where: {
-				id: token!.id,
-			},
-		})
-		.catch((err: Error) => {
-			throw new Error("Error deleting token", err);
-		});
-	return new Response(
-		JSON.stringify({
-			success: true,
-			message: "Token deleted",
-		}),
-		{
-			status: 200,
-			statusText: "Token deleted",
-		}
-	);
+  const user = await getUser();
+  const workspaceId = user?.workspaces[0]?.id;
+  if (!workspaceId) {
+    return NextResponse.json(
+      { error: "No workspaceId provided" },
+      { status: 400 },
+    );
+  }
+  const token = await prisma.token
+    .findFirst({
+      where: {
+        userId: user!.id,
+        workspaceId,
+      },
+    });
+  if (!token) {
+    return NextResponse.json(
+      { error: "Token not found" },
+      { status: 404 },
+    );
+  }
+  const deletedToken = await prisma.token
+    .delete({
+      where: {
+        id: token.id,
+      },
+    });
+  if (!deletedToken) {
+    return NextResponse.json(
+      { error: "Token not found" },
+      { status: 404 },
+    );
+  }
+  return NextResponse.json(
+    {
+      success: true,
+      message: "Token deleted",
+    },
+    {
+      status: 200,
+      statusText: "Token deleted",
+    },
+  );
 }
